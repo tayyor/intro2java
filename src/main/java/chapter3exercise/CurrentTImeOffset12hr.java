3f3262a021a00996297fc823406126c9e438b982
Haphyz Gbenle <abdulhaphyz@gmail.com>
2019-12-26 09:40:02 +0100

    excercise 3

---
+++ src/main/java/chapter3exercise/CurrentTImeOffset12hr.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter3exercise;
+
+import javax.swing.*;
+
+
+/**
+ *
+ * @author tayyor
+ */
+public class CurrentTImeOffset12hr {
+    public static void main (String args[]){
+        JFrame f = new JFrame();
+        long GMToffset = Long.parseLong(JOptionPane.showInputDialog(f, "Enter offsite value: "));
+        long GMT = System.currentTimeMillis();
+        long NewTime = GMT - GMToffset;
+        /*
+        now that the offset time has been factored in, can now begin with converting
+        the new time from secs to milli secs.
+        */
+        long TotalSeconds = NewTime /1000;
+        // now we get current seconds
+        long currentSeconds = TotalSeconds % 60;
+        // next is to get total minutes
+        long totalMinutes = TotalSeconds / 60;
+        // we then obtain the current minutes
+        long currentMinutes = totalMinutes % 60;
+        // next is to derive the total hrs
+        long totalHrs = totalMinutes /60;
+        // we then obtain the current hrs
+        long currentHrs = totalHrs % 24;
+        // Using the 12hr format;
+        currentHrs = Math.abs(currentHrs - 12);
+        JOptionPane.showMessageDialog(f,"Current time is: "+currentHrs+":"+currentMinutes+":"+currentSeconds );
+    }
+    
+}
